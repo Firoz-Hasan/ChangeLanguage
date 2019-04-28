7d6fa56d3ad7a23a5366f00e121573f70ac77526
Firoz Hasan <diganto007@gmail.com>
2019-04-28 18:13:30 +0600

    locale lang added

--- app/src/main/java/com/example/changelanguage/MainActivity.kt
+++ app/src/main/java/com/example/changelanguage/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.changelanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val mLanguageCode = "bn"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnChangeLangView?.setOnClickListener {
+            LocaleHelper().setLocale(this, mLanguageCode)
+            recreate()
+        }
     }
 }
